dc1ffc26e57ce34996b8e3927941f9f05f5033fb
James Pederson <james@jpederson.com>
2016-12-29 20:26:26 -0600

    Remove error reporting settings from functions.php.

--- functions.php
+++ functions.php
@@ -1,11 +1,6 @@
 <?php
 
 
-// error_reporting( E_ALL & ~E_NOTICE & ~E_STRICT );
-// error_reporting( E_ALL );
-// ini_set( "display_errors", 1 );
-
-
 // set a custom field prefix
 define( "CMB_PREFIX", "_p_" );
 
