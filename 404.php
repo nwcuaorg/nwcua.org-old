a4977e08558193b17665171abe43ceaddbec8ef5
James Pederson <james@jpederson.com>
2017-01-11 16:22:44 -0600

    Add some field styles and update the 404 page styles to not look so ugly.

--- 404.php
+++ 404.php
@@ -3,26 +3,29 @@
  * The template for displaying 404 pages (Not Found)
  */
 
-get_header(); ?>
+get_header(); 
 
+?>
 
 	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
-			</header>
+		<div id="content" class="site-content content-narrow" role="main">
 
 			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
+				<h1 class="page-title">Page Not Found</h1>
+				<p>It looks like nothing was found at this location. Maybe try a search?</p>
 
-				<?php get_search_form(); ?>
+				<form role="search" method="get" id="searchform" class="searchform" action="/" _lpchecked="1">
+					<input type="text" value="" name="s" id="s" placeholder="Search">
+					<input type="submit" id="searchsubmit" value="Search" class="btn-arrow">
+				</form>
+				<p>&nbsp;</p>
 			</div><!-- .page-content -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
+
 get_footer();
+
+?>
