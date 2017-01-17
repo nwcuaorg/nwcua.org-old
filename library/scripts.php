bdc4443acbeda1b86ed078df1eceb211fc624695
James Pederson <james@jpederson.com>
2017-01-16 21:39:17 -0600

    Add dashicons support.

--- library/scripts.php
+++ library/scripts.php
@@ -1,15 +1,17 @@
 <?php
 
 
+
 // include the main.js script in the header on the front-end.
 function p_scripts() {
 	wp_enqueue_script( 'p-main-js', get_stylesheet_directory_uri().'/js/main.js?ver=6', array( 'jquery' ), false, true );
+	wp_enqueue_style( 'dashicons' );
 }
 add_action( 'wp_enqueue_scripts', 'p_scripts' );
 
 
 
-
+// add page features to enable post tags and categories.
 function add_page_features() {
 
 	// add tag metabox to page
