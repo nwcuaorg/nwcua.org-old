5024bb06dd52312963aba307acdb099f20dda161
James Pederson <james@jpederson.com>
2017-01-30 20:46:58 -0600

    Add search form for events above the event calendar and set it up to display results on the events page.

--- archive-event.php
+++ archive-event.php
@@ -24,17 +24,47 @@ if ( isset( $_REQUEST['event_category'] ) && $_REQUEST['event_category']!=0 ) {
 	</div>
 	
 	<div id="content" class="wrap content-wide" role="main">
-		<p><strong>Filter by Event Type:</strong> <?php filter_by_event_type(); ?></p>
-		<br>
-		<?php 
+		<h3>Search All Events</h3>
+		<form role="search" method="get" id="searchform" class="searchform" action="/" _lpchecked="1">
+			<input type="text" value="<?php print ( isset( $_REQUEST['s'] ) ? $_REQUEST['s'] : '' ) ?>" name="s" id="s" placeholder="Search">
+			<input type="hidden" value="event" name="post_type">
+			<input type="submit" id="searchsubmit" value="Search Events" class="btn-arrow">
+		</form>
+		<hr>
+		<?php
+		if ( is_search() ) {
 
-		// get URL parameters and default to current month.
-		$month = ( isset( $_REQUEST['mo'] ) ? $_REQUEST['mo'] : date( "n" ) );
-		$year = ( isset( $_REQUEST['yr'] ) ? $_REQUEST['yr'] : date( "Y" ) );
+			if ( have_posts() ) {
+				$count = 1;
+				while ( have_posts() ) : the_post();
+					?>
+					<div class="entry entry-event">
+						<h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+						<p class="quiet"><strong>Event Date:</strong> <?php print date( 'n/j/Y \a\t g:ia', get_cmb_value( 'event_start' ) ); ?></p>
+						<?php echo wpautop( wp_trim_words( strip_tags( get_the_excerpt() ), 50 ) ); ?>
+					</div>
+					<hr />
+					<?php
+					$count++;
+				endwhile;
+			} else {
+				print "<p>Sadly, your search returned no results. Please try another or navigate using the main menu.</p>";
+			}
 
-		// output month
-		show_month_events( $month, $year );
+		} else {
+			?>
+			<h3>Browse Events</h3>
+			<p><strong>Filter by Event Type:</strong> <?php filter_by_event_type(); ?></p>
+			<br>
+			<?php 
 
+			// get URL parameters and default to current month.
+			$month = ( isset( $_REQUEST['mo'] ) ? $_REQUEST['mo'] : date( "n" ) );
+			$year = ( isset( $_REQUEST['yr'] ) ? $_REQUEST['yr'] : date( "Y" ) );
+
+			// output month
+			show_month_events( $month, $year );
+		}
 		?>
 	</div><!-- #content -->
 
