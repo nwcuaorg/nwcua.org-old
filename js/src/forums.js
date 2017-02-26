7717150c98a59c56565c0eafb8427dc966a86a56
James Pederson <james@jpederson.com>
2017-02-25 18:12:19 -0600

    Remove border from forum listings, adjust padding in various divs, and change navigation styles. Hide the first pagination div if we have more than one.

---
+++ js/src/forums.js
@@ -0,0 +1,18 @@
+
+
+jQuery(document).ready(function($){
+
+	var $forums = $('#bbpress-forums');
+
+
+	if ( $forums.length ) {
+
+		var pagination_divs = $forums.find('.bbp-pagination');
+		if ( pagination_divs.length > 1 ) {
+			pagination_divs.first().hide();
+		}
+
+	}
+
+});
+
