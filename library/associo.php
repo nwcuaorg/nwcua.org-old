30d0aa480cf3b3ce836a83a7d96c892f0f166984
James Pederson <james@jpederson.com>
2017-03-21 15:02:12 -0500

    Remove dumped data and response variable so they don't show on live site for now.

--- library/associo.php
+++ library/associo.php
@@ -18,17 +18,13 @@ function call_associo_api( $endpoint, $data='' ) {
 	) );
 
 
-
-
 	// set the data being posted to the server
 	if ( !empty( $data ) ) curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+
 
 	// execute the curl call.
 	$result = curl_exec( $ch );
 	
-	print_r( $data );
-	print_r( $result );
-	die;
 
 	// return the response
 	return $result;
