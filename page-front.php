54f22c384c585110d1a0698fc3da68c01490b2ee
James Pederson <james@jpederson.com>
2018-02-20 18:24:30 -0600

    Exclude AotM Cagegories from the homepage listing.

--- page-front.php
+++ page-front.php
@@ -88,6 +88,7 @@ if ( isset( $_REQUEST['category'] ) ) {
 		$query_args['meta_key'] = '_p_priority';
 		$query_args['orderby'] = array( 'meta_value_num' => 'DESC', 'date' => 'DESC' );
 		$query_args['posts_per_page'] = ( !empty( $events ) ? 13 : 14 );
+		$query_args['exclude'] = '7850,7851,7852,7853',
 		$query_args['meta_query'] = array(
 			array(
 				'key'=>'_p_priority',
