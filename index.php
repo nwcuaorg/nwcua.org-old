0429e71a0aa4d96049c9ec2ec17773ca6fd8cad0
James Pederson <james@jpederson.com>
2018-04-27 16:19:11 -0500

    Add same homepage filtering features to the anthem root.

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ get_header();
 ?>
 
 	
-	<div id="primary" class="site-content">
 		<?php
 
 		global $wp_query;
@@ -50,6 +49,7 @@ get_header();
 			}
 
 			?>
+	<div id="primary" class="site-content">
 		<div class="large-title bg-grey-dark">
 			<div class="wrap">
 				<div class="large-title-icon bg-grey-dark">
@@ -110,12 +110,96 @@ get_header();
 		<div class="pagination">
 			<?php pagination(); ?>
 		</div>
+	</div><!-- #primary -->
 			<?php 
 		} else {
-			?>
+
+	// get the current user, and their category preferences.
+	$the_current_user = get_current_user_id();
+	$user_categories = get_user_meta( $the_current_user, 'categories', 1 );
+
+
+	// if we have a category parameter
+	if ( isset( $_REQUEST['category'] ) ) {
+
+		// split it into an array of ids
+		$current_categories = explode( '-', $_REQUEST['category'] );
+
+		// also, update the user preference with the string from the url
+		if ( !empty( $the_current_user ) ) update_user_meta( $the_current_user, 'categories', $_REQUEST['category'] );
+
+		// set a session variable to store current session category preferences
+		$_SESSION['user_categories'] = $_REQUEST['category'];
+
+	} else if ( !empty( $user_categories ) ) {
+
+		// use the user categories if we don't have a request
+		$current_categories = explode( '-', $user_categories );
+
+	} else if ( isset( $_SESSION['user_categories'] ) ) {
+
+		// use the session variable if we don't have a request and user isn't logged in.
+		$current_categories = explode( '-', $_SESSION['user_categories'] );
+
+	}
+
+	?>
+
+	<div class="browse-by">
+		<div class="wrap">
+			<div class="browse-by-filters">
+				<form name="category-filter" action="/" method="get">
+				<div class="quarter">
+				<?php 
+				$categories = get_categories( 'exclude=1,4,20,30,31,36,38,39,42,43,44,45,47,48,49,50,51,53,54,56,1238,1276,1277,1278,1282,1284,1285,1286,1315,7571,7580,7581,7582,7585,7586,7588,7589,7590,7592,7677,7850,7851,7852,7853' );
+				$col_break = ceil( count( $categories )/4 );
+				$cnt = 1;
+				foreach ( $categories as $cat ) {
+					print '<label><input type="checkbox" name="category[]" value="' . $cat->term_id . '" '. ( isset( $current_categories ) ? ( in_array( $cat->term_id, $current_categories ) ? 'checked ' : '' ) : '' ) . '/> ' . $cat->name . '</label>';
+					if ( $cnt==$col_break || $cnt==$col_break*2 || $cnt==$col_break*3 ) print '</div><div class="quarter">';
+					$cnt++;
+				}
+				print "</div>";
+				?>
+				<div class="filter-button quarter right">
+					<input type="submit" value="Filter" />
+				</div>
+				</form>
+			</div>
+			<div class="group">
+				<a href="#" class="browse-by-handle">Browse by Category</a>
+			</div>
+		</div>
+	</div>
+	<div id="primary" class="site-content">
 		<div id="content" class="wrap content-wide home-list" role="main">
 			<?php
-			$query_args['posts_per_page'] = 14;
+
+			// get the events
+			// $events = get_upcoming_events( 3, ( isset( $_GET['category'] ) ? implode( ',', $_GET['category'] ) : 0 ) );
+			$events = get_upcoming_events( 3 );
+
+			// additional post query adjustments
+			$query_args['post_type'] = array( 'post' );
+			$query_args['meta_key'] = '_p_priority';
+			$query_args['orderby'] = array( 'meta_value_num' => 'DESC', 'date' => 'DESC' );
+			$query_args['posts_per_page'] = ( !empty( $events ) ? 13 : 14 );
+			$query_args['cat'] = '-7850,-7851,-7852,-7853,-7589,-7590';
+			$query_args['meta_query'] = array(
+				array(
+					'key'=>'_p_priority',
+					'value'=>'-1',
+					'compare'=>'!='
+				),
+			);
+
+
+			// if there was a category set in the arguments
+			if ( isset( $current_categories ) ) {
+				$query_args['cat'] = implode( ',', $current_categories );
+			}
+
+			$query_args['posts_per_page'] = ( !empty( $events ) ? 13 : 14 );
 			query_posts( $query_args );
 			if ( have_posts() ) {
 				$count = 1;
@@ -159,7 +243,26 @@ get_header();
 							<?php the_excerpt(); ?>
 						</div>
 					</div>
-					<?php
+					<?php					
+					if ( $count == 1 && !empty( $events ) ) {						
+
+						print "<div class='entry'>";
+						print "<div class='description home-events'>";
+						print "<h3><a href='https://nwcua.org/wp-content/uploads/2018/03/2018-NWCUA-Events-Handout.pdf' style='text-decoration: underline;'>2018 Events Calendar</a></h3>";
+						print "<h3><a href='/events/'>Upcoming Events</a></h3>";
+						// list the events
+						print "<div class='event-list'>";
+						foreach ( $events as $event ) {
+							print '<h4><a href="' . get_permalink( $event->ID ) . '">' . $event->post_title . '</a></h4>';
+							print "<span class='date'>" . date( 'n/j/Y g:ia', $event->_p_event_start ) . "</span>";
+						}
+						print "</div>";
+						print "</div>";
+						print "</div>";
+						$count++;
+
+					}
+
 					$count++;
 				endwhile;
 			} else {
@@ -171,10 +274,10 @@ get_header();
 		<div class="pagination group">
 			<?php pagination(); ?>
 		</div>
+	</div><!-- #primary -->
 			<?php 
 		} 
 	?>
-	</div><!-- #primary -->
 
 
 <?php 
