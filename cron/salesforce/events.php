38ffa2e522e86667c7603ac0292aadfa83f4d1c2
James Pederson <james@jpederson.com>
2019-09-19 13:51:38 -0500

    Add error reporting.

--- cron/salesforce/events.php
+++ cron/salesforce/events.php
@@ -1,6 +1,13 @@
 <?php
 
+// start the session
 session_start();
+
+
+// enable errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 // include the database class
