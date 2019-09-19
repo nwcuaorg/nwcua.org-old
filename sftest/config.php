9d917e9cf3303128b79d497d2de7d3e4bcbe33a2
James Pederson <james@jpederson.com>
2019-09-19 11:45:16 -0500

    Add rest example scripts into the theme so we can test the exact SF example code and work from there.

---
+++ sftest/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("CLIENT_ID", "3MVG9sLbBxQYwWqsFCnCBGsG1EfxOR1ZF90gO4HSUeDGNMVVHo5rPIAYloDawmTz3A6GiKEW3pZ6T2_uvqNPM");
+define("CLIENT_SECRET", "CD444AB238AB4D9A54A5E72CEDFFDF9BFB5BBF76245A12B98D78371F21EF96DE");
+define("REDIRECT_URI", "https://staging.nwcua.org/wp-content/themes/nwcua/sftest/oauth_callback.php");
+define("LOGIN_URI", "https://test.salesforce.com");
+
