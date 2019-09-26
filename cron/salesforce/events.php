b4b62a890d711d0bcf4dd991c5be6e53bb1c177b
James Pederson <james@jpederson.com>
2019-09-26 00:07:39 -0500

    Fix all field name from the API response field names we get form new SF endpoint.

--- cron/salesforce/events.php
+++ cron/salesforce/events.php
@@ -37,29 +37,33 @@ if ( empty( $access_token ) || empty( $instance_url ) ) {
 
 // get the events
 $events = get_events( $instance_url, $access_token );
-print "<pre>"; print_r( $events ); print "</pre>"; die;
+print "<pre>";
+print_r( $events );
 
-
+/*
 
 // loop through the events
-/*
 foreach ( $events as $event ) {
-
+    print_r( $event );
     if ( !empty( $event ) ) {
-        //$tz_offset = ( 3600 * ( stristr( $event->start_date, '-07:00' ) ? -1 : 0 ) );
+        //$tz_offset = ( 3600 * ( stristr( $event['EventStart__c'], '-07:00' ) ? -1 : 0 ) );
         $tz_offset = 0;
-        $timezone = ( stristr( $event->start_date, '-07:00' ) ? 'M' : 'P' );
+        $timezone = ( stristr( $event['EventStart__c'], '-07:00' ) ? 'M' : 'P' );
+
+        $event_id = ( !empty( $event['Associo_Product_ID__c'] ) ? $event['Associo_Product_ID__c'] : $event['Id'] )
+
+        $slug = sanitize_title( $event['Name'] );
 
         // get a previous post if it exists.
-        $previous_post = $db->query_one( "SELECT * FROM `nwcua_posts` WHERE `old_id`=" . $event->id . ";" );
+        $previous_post = $db->query_one( "SELECT * FROM `nwcua_posts` WHERE `old_id`=" . $event_id . ";" );
         if ( !empty( $previous_post ) ) {
             $update_query = "UPDATE `nwcua_posts` SET 
-                `post_date`=\"" . date( 'Y-m-d H:i:s', strtotime( $event->created_at ) ) . "\",
-                `post_date_gmt`=\"" . date( 'Y-m-d H:i:s', strtotime( $event->created_at ) ) . "\",
-                `post_name`=\"" . sanitize_title( $event->slug ) . "\",
-                `post_title`=\"" . $db->cn->real_escape_string( $event->name ) . "\",
-                `post_content`=\"" . $db->cn->real_escape_string( $event->content ) . "\",
-                `post_excerpt`=\"" . $db->cn->real_escape_string( $event->content ) . "\",
+                `post_date`=\"" . date( 'Y-m-d H:i:s', strtotime( $event['CreatedDate'] ) ) . "\",
+                `post_date_gmt`=\"" . date( 'Y-m-d H:i:s', strtotime( $event['CreatedDate'] ) ) . "\",
+                `post_name`=\"" . $slug . "\",
+                `post_title`=\"" . $db->cn->real_escape_string( $event['Name'] ) . "\",
+                `post_content`=\"" . $db->cn->real_escape_string( $event['Description__c'] ) . "\",
+                `post_excerpt`=\"" . $db->cn->real_escape_string( $event['Description__c'] ) . "\",
                 `post_status`=\"publish\",
                 `comment_status`=\"open\",
                 `ping_status`=\"open\",
@@ -68,46 +72,41 @@ foreach ( $events as $event ) {
                 `post_modified_gmt`=\"" . date( 'Y-m-d H:i:s' ) . "\"
                 WHERE `ID`=" . $previous_post->ID . ";";
             if ( $db->update( $update_query ) ) {
-                print 'Existing post updated: ' . $event->name . "\n";
-                print 'Time: ' . $event->start_date . "\n";
-                set_meta( $previous_post->ID, '_p_event_location_text', $event->location_text );
-                set_meta( $previous_post->ID, '_p_event_location_link', $event->location_link );
-                set_meta( $previous_post->ID, '_p_event_start', strtotime( $event->start_date )+$tz_offset );
-                set_meta( $previous_post->ID, '_p_event_end', strtotime( $event->end_date )+$tz_offset );
+                print 'Existing post updated: ' . $event["Name"] . "\n";
+                print 'Time: ' . $event['EventStart__c'] . "\n";
+                set_meta( $previous_post->ID, '_p_event_start', strtotime( $event['EventStart__c'] )+$tz_offset );
+                set_meta( $previous_post->ID, '_p_event_end', strtotime( $event['EventEnd__c'] )+$tz_offset );
                 set_meta( $previous_post->ID, '_p_event_timezone', $timezone );
-                set_meta( $previous_post->ID, '_p_event_early_date', strtotime( $event->early_price_until ) );
-                set_meta( $previous_post->ID, '_p_event_early_price', $event->early_price );
-                set_meta( $previous_post->ID, '_p_event_price', $event->price );
-                set_meta( $previous_post->ID, '_p_event_late_date', strtotime( $event->late_date ) );
-                set_meta( $previous_post->ID, '_p_event_late_price', $event->late_price );
-                if ( !empty( $event->event_type ) ) set_term( $previous_post->ID, $event->event_type );
+                // set_meta( $previous_post->ID, '_p_event_early_date', strtotime( $event->early_price_until ) );
+                // set_meta( $previous_post->ID, '_p_event_early_price', $event->early_price );
+                set_meta( $previous_post->ID, '_p_event_price', $event['Price__c'] );
+                set_meta( $previous_post->ID, '_p_event_late_date', strtotime( $event['LateDate__c'] ) );
+                // set_meta( $previous_post->ID, '_p_event_late_price', $event->late_price );
+                if ( !empty( $event['Event_Type__c'] ) ) set_term( $previous_post->ID, $event['Event_Type__c'] );
             } else {
-                print 'Existing post - failed.' . $event->name . "\n";
+                print 'Existing post - failed.' . $event['Name'] . "\n";
             }
             $post_id = $previous_post->ID;
         } else {
-            $post_id = $db->insert( "INSERT INTO `nwcua_posts` ( `post_author`, `post_modified`, `post_modified_gmt`, `post_date`, `post_date_gmt`, `post_name`, `post_title`, `post_content`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_type`, `to_ping`, `pinged`, `post_content_filtered`, `old_id` ) VALUES ( 1, \"" . date( 'Y-m-d H:i:s' ) . "\", \"" . date( 'Y-m-d H:i:s' ) . "\", \"" . date( 'Y-m-d H:i:s', strtotime( $event->created_at )-$tz_offset ) . "\", \"" . date( 'Y-m-d H:i:s', strtotime( $event->created_at ) ) . "\", \"" . sanitize_title( $event->slug ) . "\", \"" . $db->cn->real_escape_string( $event->name ) . "\", \"" . $db->cn->real_escape_string( $event->content ) . "\", \"" . $db->cn->real_escape_string( $event->content ) . "\", \"publish\", \"open\", \"open\", \"event\", '', '', '', " . $event->id . " );" );
+            $post_id = $db->insert( "INSERT INTO `nwcua_posts` ( `post_author`, `post_modified`, `post_modified_gmt`, `post_date`, `post_date_gmt`, `post_name`, `post_title`, `post_content`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_type`, `to_ping`, `pinged`, `post_content_filtered`, `old_id` ) VALUES ( 1, \"" . date( 'Y-m-d H:i:s' ) . "\", \"" . date( 'Y-m-d H:i:s' ) . "\", \"" . date( 'Y-m-d H:i:s', strtotime( $event['CreatedDate'] )-$tz_offset ) . "\", \"" . date( 'Y-m-d H:i:s', strtotime( $event['CreatedDate'] ) ) . "\", \"" . sanitize_title( $slug ) . "\", \"" . $db->cn->real_escape_string( $event['Name'] ) . "\", \"" . $db->cn->real_escape_string( $event['Description__c'] ) . "\", \"" . $db->cn->real_escape_string( $event['Description__c'] ) . "\", \"publish\", \"open\", \"open\", \"event\", '', '', '', " . $event_id . " );" );
             if ( $post_id ) {
-                print 'New event inserted: ' . $event->name . "\n";
-                print 'Time: ' . $event->start_date . "\n";
-                set_meta( $post_id, '_p_event_location_text', $event->location_text );
-                set_meta( $post_id, '_p_event_location_link', $event->location_link );
-                set_meta( $post_id, '_p_event_start', strtotime( $event->start_date )+$tz_offset );
-                set_meta( $post_id, '_p_event_end', strtotime( $event->end_date )+$tz_offset );
+                print 'New event inserted: ' . $event['Name'] . "\n";
+                print 'Time: ' . $event['EventStart__c'] . "\n";
+                set_meta( $post_id, '_p_event_start', strtotime( $event['EventStart__c'] )+$tz_offset );
+                set_meta( $post_id, '_p_event_end', strtotime( $event['EventEnd__c'] )+$tz_offset );
                 set_meta( $post_id, '_p_event_timezone', $timezone );
-                set_meta( $post_id, '_p_event_early_date', strtotime( $event->early_price_until ) );
-                set_meta( $post_id, '_p_event_early_price', $event->early_price );
-                set_meta( $post_id, '_p_event_price', $event->price );
-                set_meta( $post_id, '_p_event_late_date', strtotime( $event->late_date ) );
-                set_meta( $post_id, '_p_event_late_price', $event->late_price );
-                if ( !empty( $event->event_type ) ) set_term( $post_id, $event->event_type );
+                // set_meta( $post_id, '_p_event_early_date', strtotime( $event->early_price_until ) );
+                // set_meta( $post_id, '_p_event_early_price', $event->early_price );
+                set_meta( $post_id, '_p_event_price', $event['Price__c'] );
+                set_meta( $post_id, '_p_event_late_date', strtotime( $event['LateDate__c'] ) );
+                // set_meta( $post_id, '_p_event_late_price', $event->late_price );
+                if ( !empty( $event['Event_Type__c'] ) ) set_term( $post_id, $event['Event_Type__c'] );
             } else {
-                print 'New post - failed.' . $event->name . "\n";
+                print 'New post - failed.' . $event['Name'] . "\n";
             }
         }
     }
 }
-
 
 
 $db->update( "UPDATE nwcua_term_taxonomy SET count = (
@@ -120,6 +119,8 @@ SELECT COUNT(*) FROM nwcua_term_relationships rel
         AND 
         po.post_status IN ('publish', 'future')
 );" );
+
 */
 
 
+print "</pre>"; die;
