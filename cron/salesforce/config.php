ee7b0e33c4098bc24671566cb2d028f4125cae23
James Pederson <james@jpederson.com>
2019-11-03 16:50:19 -0600

    Adjust salesforce credentials for production.

--- cron/salesforce/config.php
+++ cron/salesforce/config.php
@@ -1,13 +1,14 @@
 <?php
 
+/*
 define( "SF_CLIENT_ID", "3MVG9sLbBxQYwWqsFCnCBGsG1EfxOR1ZF90gO4HSUeDGNMVVHo5rPIAYloDawmTz3A6GiKEW3pZ6T2_uvqNPM" );
 define( "SF_CLIENT_SECRET", "CD444AB238AB4D9A54A5E72CEDFFDF9BFB5BBF76245A12B98D78371F21EF96DE" );
 define( "SF_REDIRECT_URI", "https://staging.nwcua.org/wp-content/themes/nwcua/cron/salesforce/oauth_callback.php" );
 define( "SF_LOGIN_URI", "https://test.salesforce.com" );
+*/
 
-/*
 define( "SF_CLIENT_ID", "3MVG9i1HRpGLXp.okHFT6eI1EnzeZ7lMYbe33NVLHT6N4XVind6rMQQf6EmbD7Gh.Cwx_DTIPAhbJWbwiwSty" );
 define( "SF_CLIENT_SECRET", "13F73E617BAB2A0684A5C258B8E57C957BDB5028389A325DF62EC410BD276428" );
 define( "SF_REDIRECT_URI", "https://nwcua.org/wp-content/themes/nwcua/cron/salesforce/oauth_callback.php" );
-define( "SF_LOGIN_URI", "https://salesforce.com" );
-*/
+define( "SF_LOGIN_URI", "https://login.salesforce.com" );
+
