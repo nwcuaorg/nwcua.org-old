e14f5caca631d71c82dc1d36296cabd86011c368
James Pederson <james@jpederson.com>
2019-11-03 23:16:23 -0600

    Output session stuff to debug.

--- library/login.php
+++ library/login.php
@@ -6,7 +6,8 @@ $request = str_replace( "?" . $_SERVER['QUERY_STRING'], '',  $_SERVER['REQUEST_U
 
 // check if this is an auth request.
 if ( substr( $request, 0, 5 ) == '/auth' ) {
-	$_SESSION[ 'sf_user' ] = $_REQUEST;
+	$_SESSION['sf_user'] = $_REQUEST;
+	print_r( $_SESSION ); print session_id(); die;
 	wp_redirect( 'https://nwcua.leagueinfosight.com/admin/client/is/frontend/nwcua_sso.php?' . http_build_query( $_REQUEST ) );
 	exit;
 }
@@ -56,6 +57,7 @@ function account_button() {
 	} else {
 		?><a href="https://nwcua.force.com/s/redirect-with-url-params?url=<?php print $referer ?>" class='account button'>Log In</a><?php 
 	}
+	print "<!--" . $_SESSION . " / " . session_id() . "-->";
 
 }
 
