eb465d4e1cbd6a2cd84ee4be357f0e502f8773e3
James Pederson <james@jpederson.com>
2019-11-03 22:04:36 -0600

    Make the /logout endpoint end both WP and SF sessions.

--- library/login.php
+++ library/login.php
@@ -7,14 +7,15 @@ $request = str_replace( "?" . $_SERVER['QUERY_STRING'], '',  $_SERVER['REQUEST_U
 // check if this is an auth request.
 if ( substr( $request, 0, 5 ) == '/auth' ) {
 	$_SESSION['sf_user'] = $_REQUEST;
-	print_r( $_SESSION ); print session_id(); die;
+	// print_r( $_SESSION ); print session_id(); die;
 	wp_redirect( 'https://nwcua.leagueinfosight.com/admin/client/is/frontend/nwcua_sso.php?' . http_build_query( $_REQUEST ) );
 	exit;
 }
 
 // handle logout requests
 if ( substr( $request, 0, 7 ) == '/logout' ) {
-	unset( $_SESSION['sf_user'] );
+	// unset( $_SESSION['sf_user'] );
+	session_destroy();
 	wp_redirect( '/' );
 	exit;
 }
@@ -57,7 +58,6 @@ function account_button() {
 	} else {
 		?><a href="https://nwcua.force.com/s/redirect-with-url-params?url=<?php print $referer ?>" class='account button'>Log In</a><?php 
 	}
-	print_r( $_SESSION ); print session_id(); die;
 
 }
 
