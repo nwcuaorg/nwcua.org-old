f564a328b148f153194ba9270d4632f7c297a5a6
James Pederson <james@jpederson.com>
2019-11-26 12:09:55 -0600

    Add admin stylesheet.

--- library/scripts.php
+++ library/scripts.php
@@ -32,4 +32,10 @@ function add_page_features() {
 add_action( 'init', 'add_page_features' );
 
 
+// admin styles
+function admin_style() {
+	wp_enqueue_style('admin-styles', get_template_directory_uri().'/css/admin.css');
+}
+add_action('admin_enqueue_scripts', 'admin_style');
+
 
