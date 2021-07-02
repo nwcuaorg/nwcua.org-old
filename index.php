0cec4ea6c528b7c4bb6c008d460f2bd5493eb914
James Pederson <james@jpederson.com>
2021-07-01 19:08:27 -0500

    Remove old metaquery on anthem post list, and set 'cat' param to exclude aotm and member news.

--- index.php
+++ index.php
@@ -18,13 +18,7 @@ get_header();
 		$query_args = $wp_query->query;
 		$query_args['orderby'] = array( 'meta_value_num' => 'DESC', 'date' => 'DESC' );
 		$query_args['meta_key'] = '_p_priority';
-		$query_arts['meta_query'] = array(
-			array(
-				'key'=>'_p_priority',
-				'value'=>'-1',
-				'compare'=>'!=',
-			),
-		);
+		$query_args['cat'] = '-83,-88';
 
 		// if it's a search, display the search term.
 		if ( is_search() ) {
